5abb956728ab18d200c320556b232f04fc7cc7b2
Rohit Singh <rohit15072005@gmail.com>
2024-09-25 01:40:45 +0530

    Making initial commit

---
+++ my_app/libs/screens/login.py
@@ -0,0 +1,51 @@
+from kivymd.uix.screen import MDScreen
+from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivymd.uix.button import MDIconButton
+from kivy.animation import Animation
+
+class LoginScreen(MDScreen):
+    current_user_type = StringProperty("User")  # Default to "User"
+
+    def on_segment_change(self, user_type):
+        self.current_user_type = user_type
+        if user_type == "User":
+            self.ids.user_choice_label.text = "User"
+        elif user_type == "Admin":
+            self.ids.user_choice_label.text = "Admin"
+        elif user_type == "Dev":
+            self.ids.user_choice_label.text = "Dev"
+    
+    _once = False
+    def show_password_widget_add(self, instance):
+        if instance.text != '':
+            pass_hide_button = MDIconButton(
+                id="hide_show_pass",
+                icon="eye-off",
+                style="standard",
+                pos_hint={"right": 1.0},
+                on_press=self.show_pass,
+            )
+            if not LoginScreen._once:
+                self.ids.layout_hide_pass.add_widget(pass_hide_button)
+                self.animate_card_size(dp(300), dp(350))
+                LoginScreen._once = True
+        else:
+            if LoginScreen._once:
+                self.ids.layout_hide_pass.remove_widget(self.ids.layout_hide_pass.children[0])
+                self.animate_card_size(dp(300), dp(300))
+                LoginScreen._once = False
+        
+    def animate_card_size(self, width, height):
+        card = self.ids.login_card
+        anim = Animation(size=(width, height), duration=0.3, t='out_quad')
+        anim.start(card)
+        
+    def show_pass(self, instance):
+        instance.icon = "eye" if instance.icon == "eye-off" else "eye-off"
+        self.ids.password.password = False if instance.icon == "eye" else True
+        
+        
+    #yet to be implemented
+    def login(self, username, password, user_type):
+        self.manager.current = "home"
---
+++ my_app/main.py
@@ -0,0 +1,43 @@
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.transition import MDFadeSlideTransition
+from kivy.core.window import Window
+from kivy.lang import Builder
+
+from libs.screens.login import LoginScreen
+from libs.screens.home import HomeScreen
+
+class MyScreenManager(MDScreenManager):
+    def switch_to_login(self):
+        self.current = "login"
+
+    def switch_to_home(self):
+        self.current = "home"
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style_switch_animation = True
+        self.theme_cls.theme_style_switch_animation_duration = 0.2
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.theme_style = "Light"
+        Window.size = (340, 640)  # Common mobile phone size
+
+        # Load the KV files directly in the build method
+        Builder.load_file('libs/screens/login.kv')
+        Builder.load_file('libs/screens/home.kv')
+
+        # Create the screen instances
+        login_screen = LoginScreen()
+        home_screen = HomeScreen()
+
+        sm = MyScreenManager(transition=MDFadeSlideTransition(duration=0.1))
+        sm.add_widget(login_screen)
+        sm.add_widget(home_screen)
+        return sm
+
+    def switch_theme(self):
+        self.theme_cls.primary_palette = "Purple" if self.theme_cls.primary_palette == "Orange" else "Orange"
+        self.theme_cls.theme_style = "Dark" if self.theme_cls.theme_style == "Light" else "Light"
+
+if __name__ == '__main__':
+    MainApp().run()
